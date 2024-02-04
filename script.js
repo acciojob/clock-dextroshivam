155e1afbfec2093d04750e1c5f2f80b20268681f
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-02-04 17:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let ele=document.getElementById("timer");
+const timestamp = Date.now();
+const currentDate = new Date(timestamp);
+const formattedDate = currentDate.toLocaleString();
+ele.innerText= formattedDate;
